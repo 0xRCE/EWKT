1254d52fd89035b904b58b0e194631bb3c41f151
0xRCE <22048351+0xRCE@users.noreply.github.com>
2021-04-23 19:14:31 +0200

    Added BoundingBox + unittests

--- EWKT.Tests/BoundingBoxTests.cs
+++ EWKT.Tests/BoundingBoxTests.cs
@@ -1,18 +1,120 @@
+using EWKT.Models;
 using NUnit.Framework;
 
 namespace EWKT.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Test_BoundingBox_Inside_Expected()
         {
+            var box = new BoundingBox(0, 0, 2, 2);
+
+            Assert.IsTrue(box.Inside(new CoordinateModel { X = 1, Y = 1 }));
+            Assert.IsTrue(box.Inside(new CoordinateModel { X = 1.99, Y = 1.99 }));
         }
 
         [Test]
-        public void Test1()
+        public void Test_BoundingBox_Inside_Not_Expected()
         {
-            Assert.Pass();
+            var box = new BoundingBox(0, 0, 2, 2);
+
+            Assert.IsFalse(box.Inside(new CoordinateModel { X = 1, Y = 3 }));
+            Assert.IsFalse(box.Inside(new CoordinateModel { X = 2, Y = 2 })); //op de rand
+
+        }
+
+        [Test]
+        public void Test_BoundingBox_OnEdge_Expected()
+        {
+            var box = new BoundingBox(0, 0, 2, 2);
+
+            Assert.IsTrue(box.OnEgde(new CoordinateModel { X = 0, Y = 0 }));
+            Assert.IsTrue(box.OnEgde(new CoordinateModel { X = 1, Y = 0 }));
+            Assert.IsTrue(box.OnEgde(new CoordinateModel { X = 2, Y = 0 }));
+
+            Assert.IsTrue(box.OnEgde(new CoordinateModel { X = 2, Y = 0 }));
+            Assert.IsTrue(box.OnEgde(new CoordinateModel { X = 2, Y = 1 }));
+            Assert.IsTrue(box.OnEgde(new CoordinateModel { X = 2, Y = 2 }));
+
+            Assert.IsTrue(box.OnEgde(new CoordinateModel { X = 0, Y = 2 }));
+            Assert.IsTrue(box.OnEgde(new CoordinateModel { X = 1, Y = 2 }));
+            Assert.IsTrue(box.OnEgde(new CoordinateModel { X = 2, Y = 2 }));
+
+            Assert.IsTrue(box.OnEgde(new CoordinateModel { X = 0, Y = 0 }));
+            Assert.IsTrue(box.OnEgde(new CoordinateModel { X = 0, Y = 1 }));
+            Assert.IsTrue(box.OnEgde(new CoordinateModel { X = 0, Y = 2 }));
+        }
+
+        [Test]
+        public void Test_BoundingBox_OnEdge_Not_Expected()
+        {
+            var box = new BoundingBox(0, 0, 2, 2);
+
+            Assert.IsFalse(box.OnEgde(new CoordinateModel { X = 0.1, Y = 0.1 }));
+            Assert.IsFalse(box.OnEgde(new CoordinateModel { X = 1, Y = 1 }));
+            Assert.IsFalse(box.OnEgde(new CoordinateModel { X = 2, Y = 2.1 }));
+
+            Assert.IsFalse(box.OnEgde(new CoordinateModel { X = -0.1, Y = 0.1 }));
+            Assert.IsFalse(box.OnEgde(new CoordinateModel { X = 1, Y = 1 }));
+            Assert.IsFalse(box.OnEgde(new CoordinateModel { X = 2, Y = -0.1 }));
+
+
+            Assert.IsFalse(box.OnEgde(new CoordinateModel { X = 2, Y = 2.1 }));
+            Assert.IsFalse(box.OnEgde(new CoordinateModel { X = 2, Y = 3 }));
+            Assert.IsFalse(box.OnEgde(new CoordinateModel { X = 2, Y = -0.1 }));
+
+            Assert.IsFalse(box.OnEgde(new CoordinateModel { X = 0, Y = 2.1 }));
+            Assert.IsFalse(box.OnEgde(new CoordinateModel { X = 0, Y = 3 }));
+            Assert.IsFalse(box.OnEgde(new CoordinateModel { X = 0, Y = -0.1 }));
+        }
+
+        [Test]
+        public void Test_BoundingBox_Intersection_Expected()
+        {
+            var box = new BoundingBox(0, 0, 2, 2);
+
+            Assert.IsTrue(box.IntersectsWith(new BoundingBox(-2, -2, 0, 0)));
+            Assert.IsTrue(box.IntersectsWith(new BoundingBox(2, 0, 4, 2)));
+        }
+
+        [Test]
+        public void Test_BoundingBox_Intersection_Not_Expected()
+        {
+            var box = new BoundingBox(0, 0, 2, 2);
+
+            Assert.IsFalse(box.IntersectsWith(new BoundingBox(-2, -2, -0.000001, 0)));
+            Assert.IsFalse(box.IntersectsWith(new BoundingBox(2.0000001, 0, 4, 2)));
+        }
+
+
+        [Test]
+        public void Test_BoundingBox_Merge_Other_Smaller()
+        {
+            var box = new BoundingBox(0, 0, 4, 4);
+            var other = new BoundingBox(1, 1, 2, 2);
+
+            var merge = box.Merge(other);
+            Assert.IsNotNull(merge);
+            Assert.AreEqual(box.MaxX, merge.MaxX);
+            Assert.AreEqual(box.MinX, merge.MinX);
+            Assert.AreEqual(box.MaxY, merge.MaxY);
+            Assert.AreEqual(box.MinY, merge.MinY);
+        }
+
+
+        [Test]
+        public void Test_BoundingBox_Merge_Other_Null()
+        {
+            var box = new BoundingBox(0, 0, 4, 4);
+            var other = (BoundingBox)null;
+
+            var merge = box.Merge(other);
+            Assert.IsNotNull(merge);
+            Assert.AreEqual(box.MaxX, merge.MaxX);
+            Assert.AreEqual(box.MinX, merge.MinX);
+            Assert.AreEqual(box.MaxY, merge.MaxY);
+            Assert.AreEqual(box.MinY, merge.MinY);
         }
     }
 }
--- EWKT/BoundingBox.cs
+++ EWKT/BoundingBox.cs
@@ -1,10 +1,146 @@
-﻿using System;
+﻿using EWKT.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace EWKT
 {
     public class BoundingBox
     {
+        private CoordinateModel[] vertices;
+
+        private BoundingBox()
+        {
+            //intentionally left blank
+        }
+
+        public BoundingBox(double minX, double minY, double maxX, double maxY)
+            : this()
+        {
+            MinX = minX;
+            MaxX = maxX;
+
+            MinY = minY;
+            MaxY = maxY;
+
+            vertices = ToPolygon(this);
+        }
+
+        private CoordinateModel[] ToPolygon(BoundingBox box)
+        {
+            return new[]
+            {
+                new CoordinateModel { X = box.MinX, Y = box.MinY },
+                new CoordinateModel { X = box.MaxX, Y = box.MinY },
+                new CoordinateModel { X = box.MaxX, Y = box.MaxY },
+                new CoordinateModel { X = box.MinX, Y = box.MaxY },
+                new CoordinateModel { X = box.MinX, Y = box.MinY }
+            };
+        }
+
+        public double MinX { get; set; }
+        public double MaxX { get; set; }
+        public double MinY { get; set; }
+        public double MaxY { get; set; }
+
+        public bool IntersectsWith(BoundingBox other)
+        {
+            var otherVertices = ToPolygon(other);
+            return otherVertices.Any(Inside) || otherVertices.Any(OnEgde);
+        }
+
+        public bool OnEgde(CoordinateModel p)
+        {
+            for (int i = 0, j = 1; j < vertices.Length; i++, j++)
+            {
+                var v1 = vertices[i];
+                var v2 = vertices[j];
+                bool locatedOnEdge = OnEdge(p, v1, v2);
+
+                if (locatedOnEdge)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public BoundingBox Merge(BoundingBox other)
+        {
+            if (other == null)
+            {
+                return this;
+            }
+
+            var bounds = new BoundingBox();
+            bounds.MinX = Math.Min(MinX, other.MinX);
+            bounds.MaxX = Math.Max(MaxX, other.MaxX);
+
+            bounds.MinY = Math.Min(MinY, other.MinY);
+            bounds.MaxY = Math.Max(MaxY, other.MaxY);
+
+            bounds.vertices = ToPolygon(bounds);
+            return bounds;
+        }
+
+        private bool OnEdge(CoordinateModel p, CoordinateModel v1, CoordinateModel v2)
+        {
+            var dy = v2.Y - v1.Y;
+
+            if (Equals(dy, 0d)) //horizontal line (p.x must be within (including) x interval v1 and v2
+            {
+                var xMin = Math.Min(v1.X, v2.X);
+                var xMax = Math.Max(v1.X, v2.X);
+                return Equals(p.Y, v1.Y) && (xMin <= p.X && p.X <= xMax);
+            }
+
+            var dx = v2.X - v1.X;
+            if (Equals(dx, 0d)) //vertical line (p.y must be within (including) y interval v1 and v2
+            {
+                var yMin = Math.Min(v1.Y, v2.Y);
+                var yMax = Math.Max(v1.Y, v2.Y);
+                return Equals(p.X, v1.X) && (yMin <= p.Y && p.Y <= yMax);
+            }
+
+            //fail-safe, should not occur, because the boundingbox is a rectangle
+
+            //solve f(x) = ax + b for p
+            var a = dy / dx;
+            var y = a * (p.X - v1.X) + v1.Y;
+            return Equals(y, p.Y);
+        }
+
+        private bool Equals(double a, double b)
+        {
+            const double ypsilon = 0.00000000001;
+            return Math.Abs(a - b) <= ypsilon;
+        }
+
+        public bool Inside(CoordinateModel p)
+        {
+            //point is outside the rectangular representation of this instance
+            if (p.X < MinX || p.X > MaxX || p.Y < MinY || p.Y > MaxY)
+            {
+                return false;
+            }
+
+            // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+            bool inside = false;
+            for (int i = 0, j = vertices.Length - 1; i < vertices.Length; j = i++)
+            {
+                var vi = vertices[i];
+                var vj = vertices[j];
+                if ((vi.Y > p.Y) != (vj.Y > p.Y) &&
+                     p.X < (vj.X - vi.X) * (p.Y - vi.Y) / (vj.Y - vi.Y) + vi.X)
+                {
+                    inside = !inside;
+                }
+            }
+
+            return inside;
+        }
+
     }
 }
